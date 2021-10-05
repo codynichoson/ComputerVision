c06cbe29c9941e95e19a381dd9ddffefeb32ddfc
Cody Nichoson <codynichoson@gmail.com>
2021-10-05 15:47:43 -0500

    Successful CCL for face.bmp image

--- MP1/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-for x in range(height):
-#     for y in range(width):
-#         if img.getpixel((125,115)) == 255:
-#             thing[x][y] = 1.0
-#         elif thing[x][y] == False:
-#             thing[x][y] = 0.0
