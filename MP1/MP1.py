b6994764a5d1c6865b95b00ce69c7776530d310a
Cody Nichoson <codynichoson@gmail.com>
2021-10-05 16:11:09 -0500

    Added comments

--- MP1/MP1.py
+++ MP1/MP1.py
@@ -6,12 +6,14 @@ import numpy as np
 import sys
 np.set_printoptions(threshold=sys.maxsize)
 
+# Import images
 img1 = Image.open('/home/codynichoson/computer_vision/MP1/test.bmp')
 img2 = Image.open('/home/codynichoson/computer_vision/MP1/face.bmp')
 img3 = Image.open('/home/codynichoson/computer_vision/MP1/gun.bmp')
 img4 = Image.open('/home/codynichoson/computer_vision/MP1/face_old.bmp')
 
 def run_ccl(img):
+    # Initialize variables
     E_table = []
     label = 0
     width, height = img.size
@@ -23,9 +25,9 @@ def run_ccl(img):
             if img.getpixel((column,row)) == 255:
                 L_upper = binary_arr[row][column-1] # Upper label
                 L_left = binary_arr[row-1][column] # Left label
-                if L_upper == L_left and L_upper != 0 and L_left != 0: # Same label
+                if L_upper == L_left and L_upper != 0 and L_left != 0:
                     binary_arr[row][column] = L_upper
-                elif L_upper != L_left and (L_upper == 0 or L_left == 0): # Left neighbor
+                elif L_upper != L_left and (L_upper == 0 or L_left == 0):
                     binary_arr[row][column] = max(L_upper, L_left)
                 elif L_upper != L_left and L_upper > 0 and L_left > 0:
                     binary_arr[row][column] = min(L_upper, L_left)
@@ -41,20 +43,23 @@ def run_ccl(img):
         E_table[E_table.index(x)] = sorted(x, reverse=True) # Put pairs in order (larger then smaller)
     E_table.sort(reverse=True) # Sort by first element (reverse order)
 
+    # Second pass to apply equal pairs from E_table
     for row in range(height):
         for column in range(width):
-            if binary_arr[row][column] != 0:
-                for pair in E_table:
-                    if binary_arr[row][column] == pair[0]:
-                        binary_arr[row][column] = pair[1]
+            if binary_arr[row][column] != 0: # If the pixel has value other than 0
+                for pair in E_table: # Check E_table
+                    if binary_arr[row][column] == pair[0]: # If the current pixel equals the first number in E pair
+                        binary_arr[row][column] = pair[1] # Then change pixel value to second number in E pair
 
     return binary_arr
 
+# Run CCL function for all four images
 binary_arr1 = run_ccl(img1)
 binary_arr2 = run_ccl(img2) 
 binary_arr3 = run_ccl(img3) 
 binary_arr4 = run_ccl(img4) 
 
+# Plot resulting array for each image
 plot1 = plt.figure(1)
 img_show = plt.imshow(binary_arr1)
 plot2 = plt.figure(2)
