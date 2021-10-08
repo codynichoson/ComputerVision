de1c0265f0d07acaf503ee24d014c9a6866b53a1
Cody Nichoson <codynichoson@gmail.com>
2021-10-07 20:47:43 -0500

    Finished MP2 Morphological Operators

--- 2 - Morphological Operators/MP2.py
+++ 2 - Morphological Operators/MP2.py
@@ -12,11 +12,13 @@ import matplotlib.pyplot as plt
 np.set_printoptions(threshold=sys.maxsize)
 
 # Import images
-img1 = cv.imread('/home/codynichoson/computer_vision/MP2/gun.bmp',0)
+#img1 = cv.imread('/home/codynichoson/computer_vision/MP2/gun.bmp',0)
+img1 = cv.imread('/home/codynichoson/computer_vision/MP2/palm.bmp',0)
 height = img1.shape[0]
 width = img1.shape[1]
 img1_bin = img1/255
 
+# Plot original image
 plt.subplot(3, 3, 1)
 plt.figure(1)
 plt.imshow(img1_bin)
@@ -64,20 +66,6 @@ def erosion(img, size):
 
     return new_img
 
-def get_element(img, r, c):
-    element = np.array([[0,0,0],[0,0,0],[0,0,0]])
-    element[1][1] = img[r][c]
-    element[0][1] = img[r-1][c]
-    element[2][1] = img[r+1][c]
-    element[1][0] = img[r][c-1]
-    element[1][2] = img[r][c+1]
-    element[2][2] = img[r+1][c+1]
-    element[0][0] = img[r-1][c-1]
-    element[2][0] = img[r+1][c-1]
-    element[0][2] = img[r-1][c+1]
-
-    return element
-
 def opening(img, SE):
     x = erosion(img, SE)
     open_img = dilation(x, SE)
@@ -97,6 +85,21 @@ def boundary(img):
 
     return bound_img
 
+def get_element(img, r, c):
+    element = np.array([[0,0,0],[0,0,0],[0,0,0]])
+    element[1][1] = img[r][c]
+    element[0][1] = img[r-1][c]
+    element[2][1] = img[r+1][c]
+    element[1][0] = img[r][c-1]
+    element[1][2] = img[r][c+1]
+    element[2][2] = img[r+1][c+1]
+    element[0][0] = img[r-1][c-1]
+    element[2][0] = img[r+1][c-1]
+    element[0][2] = img[r-1][c+1]
+
+    return element
+
+# Run the functions
 dil3_img = dilation(img1,3)
 dil5_img = dilation(img1,5)
 ero3_img = erosion(img1,3)
@@ -106,6 +109,7 @@ open5_img = opening(img1,5)
 close3_img = closing(img1,3)
 bound_img = boundary(img1)
 
+# Plot the images
 plt.subplot(3, 3, 2)
 plt.imshow(dil3_img)
 plt.title("Dilation 3x3")
---
+++ 2 - Morphological Operators/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+def erosion(img, size):
+#     se = np.ones((size,size))
+
+   
+
+#     d = size // 2
+
+#     height = img.shape[0]
+#     width = img.shape[1]
+
+#     new_img = np.zeros((height,width))
+
+    
+
+#     plt.figure(1)
+#     img_show = plt.imshow(new_img)
